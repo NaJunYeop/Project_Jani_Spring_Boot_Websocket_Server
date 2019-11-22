51053d032c9225acc99214be7f279a2ebf35cd5d
NaJunYeop <nnjy1992@naver.com>
2019-11-22 18:59:18 +0900

    IS_DB_Connection_Refactoring_Complete

--- WebSocketServer/webSocketServer/src/main/java/com/example/demo/controller/MySQLController.java
+++ WebSocketServer/webSocketServer/src/main/java/com/example/demo/controller/MySQLController.java
@@ -11,7 +11,6 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import com.example.demo.model.DBRepository;
-import com.example.demo.model.MessageModel;
 import com.example.demo.model.RegisterModel;
 import com.example.demo.model.UserInformation;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -19,17 +18,23 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
-public class WebSocketController {
-	private static Logger logger = LoggerFactory.getLogger(WebSocketController.class);
+public class MySQLController {
+	private static Logger logger = LoggerFactory.getLogger(MySQLController.class);
+	
+	@Autowired
+	private DBRepository dbRepository;
 	
 	private ObjectMapper mapper = new ObjectMapper();
-	private MessageModel messageModel;
-
-	@MessageMapping("/end")
+	private RegisterModel registerModel;
+	
+	private Optional<UserInformation> queryRegister;
+	private List<UserInformation> queryList;
+	
+	@MessageMapping("/db-register")
 	@SendTo("/topic/greetings") // @SendTo안의 Subscriber에게 전송하기 위해 MessageBroker로 전송한다.
-	public String sendMessage(String json) {
+	public String userRegistration(String json) {
 		try {
-			messageModel = mapper.readValue(json, MessageModel.class);
+			registerModel = mapper.readValue(json, RegisterModel.class);
 		} catch (JsonMappingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -37,6 +42,19 @@ public class WebSocketController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return json;
+		
+		queryRegister = dbRepository.findByUserNameAndPhoneNumber(registerModel.getUserName(), registerModel.getPhoneNumber());
+		if (!queryRegister.isPresent()) {
+			UserInformation userInfo = new UserInformation.Builder(registerModel.getUserName(), registerModel.getPhoneNumber())
+					.setEmail(registerModel.getEmail())
+					.build();
+			dbRepository.save(userInfo);
+			logger.info(registerModel.toString());
+			return "등록이 완료되었습니다." + registerModel.toString();
+		}
+		else {
+			logger.info("Account Already Exists....\n");
+			return "이미 존재하는 계정입니다.";
+		}		
 	}
 }
---
+++ WebSocketServer/webSocketServer/src/main/java/com/example/demo/model/MessageModel.java
@@ -0,0 +1,14 @@
+package com.example.demo.model;
+
+public class MessageModel {
+	private String userName;
+	private String contents;
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+	public String getContents() {
+		return contents;
+	}
+}
