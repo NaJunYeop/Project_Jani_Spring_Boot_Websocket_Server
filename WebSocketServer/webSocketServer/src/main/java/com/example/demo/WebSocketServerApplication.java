52a8132cf90eae3247352a8ebc53a6a80d538086
Lucas-one <first1373@naver.com>
2020-03-09 22:41:46 +0900

    IS_Topic_Number

--- WebSocketServer/webSocketServer/src/main/java/com/example/demo/WebSocketServerApplication.java
+++ WebSocketServer/webSocketServer/src/main/java/com/example/demo/WebSocketServerApplication.java
@@ -1,11 +1,10 @@
 package com.example.demo;
 
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.example.demo.models.UserInformationEntity;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example.demo")
